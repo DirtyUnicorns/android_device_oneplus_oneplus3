883dc648aa01f963a369d7d0f98506a14c527bba
Diego Wilson <diegowilson@google.com>
2017-02-24 17:07:28 -0800

    Move device dependent modules to /vendor
    Test: lunch marlin; make vendorimage;
    fastboot -w flash vendor vendor.img # on marlin
    Change-Id: Iecbcde5a0ce6470c0dc9d82df5e6581c63ad7a7f

--- device.mk
+++ device.mk
@@ -107,13 +107,13 @@ PRODUCT_PACKAGES += \
     libgnss \
     libgnsspps
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/flp.conf:system/vendor/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/gps.conf:system/vendor/etc/gps.conf \
-    $(LOCAL_PATH)/gps/etc/izat.conf:system/vendor/etc/izat.conf \
-    $(LOCAL_PATH)/gps/etc/lowi.conf:system/vendor/etc/lowi.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:system/vendor/etc/sap.conf \
-    $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/vendor/etc/xtwifi.conf
+PRODUCT_PACKAGES += \
+    flp.conf \
+    gps.conf \
+    izat.conf \
+    lowi.conf \
+    sap.conf \
+    xtwifi.conf
 
 # IMS
 PRODUCT_PACKAGES += \
