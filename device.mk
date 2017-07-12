696c10794a83964afe6e5226d1dd378c0f215eab
padarshr <padarshr@codeaurora.org>
2017-07-12 16:46:43 +0530

    op3: Update vndk-sp build mechanism and library set.
    This change is to add some more libraries needed to
    be present in the vndk-sp library set. Also, move towards
    a better the mechanism to build vndk-sp.
    
    Change-Id: Iac86c8ece08c2dae94fce6e94deb670aa742e3dc

--- device.mk
+++ device.mk
@@ -276,25 +276,9 @@ $(call inherit-product, $(LOCAL_PATH)/treble.mk)
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
 
-# Vndk-sp
+# VNDK-SP:
 PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0.vndk-sp\
-    android.hardware.graphics.allocator@2.0.vndk-sp\
-    android.hardware.graphics.mapper@2.0.vndk-sp\
-    android.hardware.graphics.common@1.0.vndk-sp\
-    android.hidl.base@1.0.vndk-sp\
-    libhwbinder.vndk-sp\
-    libbase.vndk-sp\
-    libcutils.vndk-sp\
-    libhardware.vndk-sp\
-    libhidlbase.vndk-sp\
-    libhidltransport.vndk-sp\
-    libutils.vndk-sp\
-    libc++.vndk-sp\
-    libsync.vndk-sp\
-    libbacktrace.vndk-sp\
-    libunwind.vndk-sp\
-    liblzma.vndk-sp\
+    vndk-sp
 
 # VR
 PRODUCT_PACKAGES += \
