6d82db994a22d8954e036d934759e935ca22e106
Bruno Martins <bgcngm@gmail.com>
2018-09-16 07:24:05 -0500

    Convert lights HAL into a native binderized HAL
    Change-Id: If595777978b0224dfe187b5f033f6654b970b770

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -68,7 +68,7 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.light@2.0-service.oneplus3
 
 # Net
 PRODUCT_PACKAGES += \
