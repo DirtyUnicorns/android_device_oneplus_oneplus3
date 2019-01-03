9c2cc0b2aa52fe2d6072760d1df6019eb4733e91
Alex Cruz <du.alexcruz@gmail.com>
2019-01-03 04:48:06 -0600

    Remove TrustZone assertion

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,8 +31,7 @@ TARGET_SPECIFIC_HEADER_PATH := $(PLATFORM_PATH)/include
 BOARD_VENDOR := oneplus
 
 # Assertions
-TARGET_BOARD_INFO_FILE ?= $(PLATFORM_PATH)/board-info.txt
-TARGET_OTA_ASSERT_DEVICE := OnePlus3,oneplus3,OnePlus3T,oneplus3t
+TARGET_OTA_ASSERT_DEVICE := none
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := msm8996
