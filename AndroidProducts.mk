05ec22de4a8d3b1c94655797bb741aa8a59d4493
Alex Koskovich <zvnexus@outlook.com>
2019-10-16 09:11:15 -0400

    Dirty Unicorns
    Change-Id: I65270354048aeed4f0fa7b4caf1bf5a1814610dd

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2019 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_oneplus3.mk
+    $(LOCAL_DIR)/du.mk
--- du.mk
+++ du.mk
@@ -1,5 +1,4 @@
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2019 The Dirty Unicorns Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,26 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit from those products. Most specific first.
+
+# AOSP
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from oneplus3 device
+# DU
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+
+# Device
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Vendor security patch level
-VENDOR_SECURITY_PATCH := 2019-04-01
-
-PRODUCT_NAME := lineage_oneplus3
+PRODUCT_NAME := du_oneplus3
 PRODUCT_DEVICE := oneplus3
-PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-
-PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+PRODUCT_MANUFACTURER := OnePlus
 
 BUILD_FINGERPRINT := "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys"
-
-TARGET_VENDOR := oneplus
